ae8774825f45c535adb3702816ad993493059db9
Jalil F <jrab227@gmail.com>
2021-09-30 23:28:12 +0000

    Fixing test.

--- tests/test_BloodOath.py
+++ tests/test_BloodOath.py
@@ -13,12 +13,6 @@ def test_initial_state_takers(bloodoath_instance, accounts):
     assert bloodoath_instance.takers(0) == accounts[0].address, "First taker not correctly set."
     assert bloodoath_instance.takers(1) == accounts[1].address, "First taker not correctly set."
 
-def test_initial_state_takers(bloodoath_instance, accounts):
-    """Test if contract initial state was set for given oath takers.
-    """
-    assert bloodoath_instance.takers(0) == accounts[0].address, "First taker not correctly set."
-    assert bloodoath_instance.takers(1) == accounts[1].address, "First taker not correctly set."
-
 def test_initial_state_lockEnd(bloodoath_instance):
     """Test if correct lock end was set during initialization.
     """
